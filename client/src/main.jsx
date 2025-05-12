ce5b740e22f5aa9b17a0de8582330f47b229c8cf
shefket-mustafa <miracle_1@abv.bg>
2025-05-12 20:05:13 +0300

    react router installed and app wrapped around it

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import './style.css'
+import { BrowserRouter } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
+  </BrowserRouter>
 )
