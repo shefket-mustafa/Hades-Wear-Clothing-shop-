75dad970df41ec30752f9d7e029a306e9ef81cb8
shefket-mustafa <miracle_1@abv.bg>
2025-05-13 19:22:39 +0300

    Small bug adjusted

---
+++ client/src/components/home/top-container/TopContainer.jsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router';
+import homeTop from '../../../assets/images/home-main-top.webp'
+
+
+export default function TopContainer() {
+  return <div className='top-container'>
+  <img className='home-top-img' src={homeTop} alt="Home top image..." />
+  <Link className='top-for-her'>FOR HER</Link>
+  </div>;
+}
