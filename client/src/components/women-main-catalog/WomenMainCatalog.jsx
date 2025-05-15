0489aebb7144d44e46d237aebdc593e0358a637a
shefket-mustafa <miracle_1@abv.bg>
2025-05-15 22:19:29 +0300

    Womens catalog component created

---
+++ client/src/components/women-main-catalog/WomenMainCatalog.jsx
@@ -0,0 +1,13 @@
+import { useLocation } from "react-router";
+
+export default function WomenMainCatalog() {
+
+
+  return <div className="women-main-catalog">
+
+    <div className="top-women-catalog">
+        <h1>WOMENS</h1>
+    </div>
+
+  </div>;
+}
