832199552aac564476f104ac2eff9509003cd33e
shefket-mustafa <miracle_1@abv.bg>
2025-05-15 22:13:18 +0300

    Scroll on top feature applied

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,18 +3,23 @@ import CounterHeader from "./components/counter-header/CounterHeader"
 import Home from "./components/home/Home"
 import Footer from './components/footer/Footer.jsx';
 import { Route, Routes } from "react-router";
+import WomenMainCatalog from "./components/women-main-catalog/WomenMainCatalog.jsx";
+import Scroll from "./components/scroll-ontop/Scroll.jsx";
 
 
 function App() {
 
   return (
     <>
+    <Scroll />
       <CounterHeader />
       <Header />
 
     <Routes>
       <Route path="/" element={<Home />}/>
+      <Route path="/catalog/women" element={<WomenMainCatalog />}/>
 
+      <Route path="*" element={<div>404 Not Found</div>} />
     </Routes>
       <Footer />
     </>
