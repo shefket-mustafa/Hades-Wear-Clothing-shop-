112ee23b148068a0a928b2b0d29f575e6f11e13b
shefket-mustafa <miracle_1@abv.bg>
2025-05-16 00:07:34 +0300

    Scaling added to items on hover and some styling made

--- client/src/components/women-main-catalog/women-catalog-item/WomenCatalogItem.jsx
+++ client/src/components/women-main-catalog/women-catalog-item/WomenCatalogItem.jsx
@@ -2,8 +2,14 @@ import './womenCatalogItem.css'
 
 export default function WomenCatalogItem({product}) {
   return <div className='women-catalog-item'>
+    <div className='women-item-img'>
     <img src={product.images[0]} alt={product.title} />
+    </div>
+    <div className='women-catalog-item-p'>
     <p>{product.title}</p>
     <p>{product.price}</p>
+    </div>
+    <div>
+        </div>
   </div>;
 }
