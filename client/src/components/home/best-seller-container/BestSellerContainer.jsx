1521cf1f3e3897968b91ccdc569167060fc0617e
shefket-mustafa <miracle_1@abv.bg>
2025-05-15 22:17:59 +0300

    Small ajdustments made to layout

--- client/src/components/home/best-seller-container/BestSellerContainer.jsx
+++ client/src/components/home/best-seller-container/BestSellerContainer.jsx
@@ -1,20 +1,30 @@
 
 import { useEffect, useState } from "react";
 import Bestsellers from "../bestsellers/Bestsellers";
-import { useGetBestSellers } from "../../../api-hooks/api-hooks";
+import { useGetAllWomensItems, useGetBestSellers } from "../../../api-hooks/api-hooks";
 
 export default function BestSellerContainer() {
 
     const [data, setData] = useState([]);
     const {getBestSellers} = useGetBestSellers();
+    const {getAllWomenItems} = useGetAllWomensItems();
 
     useEffect(() => {
         getBestSellers()
         .then(products => {
             setData(products)
+            
+        });
+    },[])
+
+    useEffect(() => {
+        getAllWomenItems()
+        .then(products => {
             console.log(products);
         });
     },[])
+
+
   return <div className='bestseller-container'>
   <div className='title'><h1>BESTSELLERS</h1></div>
   <div className='bestsellers'>
