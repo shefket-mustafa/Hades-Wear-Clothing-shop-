052deedeaa47383cf419030705fdc25cac24f321
shefket-mustafa <miracle_1@abv.bg>
2025-05-16 22:00:32 +0300

    Timer added for end of sale (it restarts on refresh but i will probably use localstorage to make it persist later on)

--- client/src/components/counter-header/CounterHeader.jsx
+++ client/src/components/counter-header/CounterHeader.jsx
@@ -1,6 +1,29 @@
+import { useEffect, useState } from 'react';
 import './counter-header.css';
 
 export default function CounterHeader() {
+
+  const [secondsLeft, setSecondsLeft] = useState(10 * 60); 
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSecondsLeft(prev => {
+        if (prev <= 1) {
+          clearInterval(interval);
+          return 0;
+        }
+        return prev - 1;
+      });
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const formatTime = (seconds) => {
+    const m = String(Math.floor(seconds / 60)).padStart(2, '0');
+    const s = String(seconds % 60).padStart(2, '0');
+    return `${m}:${s}`;
+  };
   return <div className="header-counter-container">
 
     <div className="header-discount-text">
@@ -9,8 +32,8 @@ export default function CounterHeader() {
     </div>
 
     <div className="header-counter">
-        <p>ENDS IN:</p>
-        <p>Counter</p>
+        <p className='ends-timer'>ENDS IN:</p>
+        <p className='timer'>{secondsLeft === 0 ? "Offer expired" : formatTime(secondsLeft)}</p>
     </div>
 
   </div>;
