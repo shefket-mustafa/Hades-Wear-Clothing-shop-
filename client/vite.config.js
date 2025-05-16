d23a4cab4a96e68a7643c1f4557ffc29014370c9
shefket-mustafa <miracle_1@abv.bg>
2025-05-16 13:57:39 +0300

    Trying to fix deployment issues

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['react-router'] // ← prevents pre-bundling issues
-  },
-  ssr: {
-    noExternal: ['react-router'] // ← prevents SSR resolution failures
-  }
 })
