67e2c14f198fa829e811655f52fc27c0fdf61bc6
shefket-mustafa <miracle_1@abv.bg>
2025-05-22 23:06:20 +0300

    Routing added to bestseller items

--- client/src/components/home/bestsellers/Bestsellers.jsx
+++ client/src/components/home/bestsellers/Bestsellers.jsx
@@ -1,13 +1,15 @@
+import { useNavigate } from 'react-router';
 import './bestsellers.css'
 
 export default function Bestsellers({itemData}) {
 
+  const navigate = useNavigate();
     
 
 
   return <div className="bestseller-item">
     <div className='bestseller-top'>
-    <img src={itemData.thumbnail} alt={itemData.title} />
+    <img onClick={() => navigate(`/catalog/${itemData.id}/details`)} src={itemData.thumbnail} alt={itemData.title} />
     </div>
 
     <div className="product-data">
