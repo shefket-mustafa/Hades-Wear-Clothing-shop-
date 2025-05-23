fa7eb1871783585ed6ccb93de92fa029645d24a9
shefket-mustafa <miracle_1@abv.bg>
2025-05-23 18:22:35 +0300

    Cart component created and styled

--- client/src/components/cart/Cart.jsx
+++ client/src/components/cart/Cart.jsx
@@ -1,5 +1,25 @@
-export default function Cart() {
-  return <div>
+import './cart.css'
+
+export default function Cart({ cartItems = [] }) {
+  return <div className="cart-modal">
+
+    <div className="cart-items-left">
+
+      <h2>Your items</h2>
+      {cartItems.length === 0 ? <p className='cart-empty'>Your shopping cart is empty.</p> : 
+      
+      cartItems.map((item) => (
+      <div key={item.id} className='cart-left-item'>
+        <img src={item.images[0]} alt={item.title} />
+        <div className='cart-item-details'>
+          <p>{item.title}</p>
+          <p>{item.price} €</p>
+        </div>
+      </div>
+      ))
+      }
+
+    </div>
     
   </div>;
 }
